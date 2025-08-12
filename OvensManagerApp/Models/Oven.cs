3f61bf015cbdd95f0e70cec3a8f660c9dd041646
basemcode <116451311+basemcode@users.noreply.github.com>
2025-08-12 22:10:22 +0300

    Add OvensManagerApp WPF project to solution
    Added a new WPF application project, OvensManagerApp, targeting .NET 8.0. Included initial app structure with App.xaml, MainWindow, and AssemblyInfo. Added Models/Oven class and a placeholder Services/OvenDataService.

---
+++ OvensManagerApp/Models/Oven.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OvensManagerApp.Models;
+
+public class Oven
+{
+    public int Number { get; set; }
+    public byte Address { get; set; }
+    public int TargetTemp { get; set; }
+    public int OpeningTemp { get; set; }
+}
